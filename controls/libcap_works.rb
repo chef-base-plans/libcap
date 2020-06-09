82271fa90e351e9c071593a726b38e32930d0e4a
Steven Marshall <SMarshall@chef.io>
2020-06-09 14:15:12 +0100

    [libcap] Initial plan and tests
    Signed-off-by: Steven Marshall <SMarshall@chef.io>

---
+++ controls/libcap_works.rb
@@ -0,0 +1,42 @@
+title 'Tests to confirm libcap binaries work as expected'
+
+base_dir = input("base_dir", value: "bin")
+plan_origin = ENV['HAB_ORIGIN']
+plan_name = input("plan_name", value: "libcap")
+plan_ident = "#{plan_origin}/#{plan_name}"
+
+control 'core-plans-libarchive' do
+  impact 1.0
+  title 'Ensure libarchive binaries are working as expected'
+  desc '
+  We check that each of the binaries libcap installs exist and then run checks on them to verify that they are excecutable.
+  '
+  
+  capsh_works = command("/bin/capsh --help")
+  describe capsh_works do
+    its('stdout') { should match /usage: \/bin\/capsh/ }
+    its('stderr') { should be_empty }
+    its('exit_status') { should eq 0 }
+  end
+
+  getcap_works = command("/bin/getcap -h")
+  describe getcap_works do
+    its('stdout') { should be_empty }
+    its('stderr') { should match /usage: getcap/ }
+    its('exit_status') { should eq 1 }
+  end
+
+  getpcaps_works = command("/bin/getpcaps")
+  describe getpcaps_works do
+    its('stdout') { should be_empty }
+    its('stderr') { should match /usage: getcaps/ }
+    its('exit_status') { should eq 1 }
+  end
+
+  setcap_works = command("/bin/setcap --help")
+  describe setcap_works do
+    its('stdout') { should be_empty }
+    its('stderr') { should match /usage: setcap/ }
+    its('exit_status') { should eq 1 }
+  end
+end
